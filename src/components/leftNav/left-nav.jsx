b61b7762d65cf619f21f2bcfb93805d0ccd63d76
Tuz1 <523504022@qq.com>
2019-01-16 01:20:25 +0800

    '3'

--- src/components/leftNav/left-nav.jsx
+++ src/components/leftNav/left-nav.jsx
@@ -11,13 +11,54 @@ import './left-nav.less'
  */
 
 const SubMenu = Menu.SubMenu
+const Item = Menu.Item
 
-export default class LeftNav extends Component {
+class LeftNav extends Component {
+
+  getNodes = (list) => {
+    return list.reduce((pre,item) => {
+      if(item.children){
+        const subMenu = (
+          <SubMenu key={item.key} title={<span><Icon type={item.icon}/><span>{item.title}</span></span>}>
+            {
+              this.getNodes(item.children)
+            }
+          </SubMenu>
+        )
+        pre.push(subMenu)
+      } else {
+        const menuItem = (
+          <Item key={item.key}>
+            <NavLink to={item.key}>
+              <Icon type={item.icon}/> {item.title}
+            </NavLink>
+          </Item>
+        )
+        pre.push(menuItem)
+      }
+      return pre
+    },[])
+  }
+
+  componentWillMount(){
+    this.menuNodes = this.getNodes(menuList)
+  }
+
   render() {
+    const path = this.props.location.pathname
     return (
       <div className='left-nav'>
-        LeftNav
+        <NavLink to='/home' className='logo'>
+          <img src={logo} alt="logo"/>
+          <h1>硅谷后台</h1>
+        </NavLink>
+
+        <Menu mode="inline" theme='dark' defaultSelectedKeys={[path]}>
+          {this.menuNodes}
+        </Menu>
       </div>
     )
   }
 }
+
+export default withRouter(LeftNav)
